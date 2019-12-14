c6232d9db6a615cfefc5ce678072f23d42c5685c
Xzio <1024508706@qq.com>
2019-12-14 14:06:08 +0800

    安全部配置

---
+++ eureka/src/main/java/com/example/eureka/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author: xc
+ * @Date: 2019/12/12 17:11
+ * @Description:
+ **/
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
